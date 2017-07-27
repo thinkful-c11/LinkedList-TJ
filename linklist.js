c9094e141a809928a87414cf00fa872fc900a349
tannaurus <contacttannergill@gmail.com>
2017-07-27 11:31:07 -0700

    Improved multiple functions which no longer rely on the length property

--- linklist.js
+++ linklist.js
@@ -65,27 +65,41 @@ class LinkedList {
 
 const display = (linklist) => {
   let node = linklist.head;
-  for (let i = 0; i < linklist.length; i++) {
+  while (node.next !== null) {
     console.log(node.value);
     node = node.next;
   }
 };
 
 const size = (linklist) => {
-  console.log(linklist.length);
+  let node = linkedList.head;
+  let counter = 0;
+  while (node.next !== null){
+    node = node.next;
+    counter++;
+  }
+  return counter;
 };
 
 const isEmpty = (link) => {
-  if(!link.length) return true;
+  if(link.head === null) return true;
   return false;
 };
 
-const findPrevious = (link, index) => {
-  console.log(link.get(index - 1));
+const findPrevious = (link, value) => {
+  let node = link.head;
+  while(node.next !== null){
+    if(node.next.value === value) return node;
+    node = node.next;
+  }
 };
 
 const findLast = (link) => {
-  console.log(link.get(link.length - 1));
+  let node = link.head;
+  while(node.next !== null){
+    node = node.next;
+  }
+  return node;
 };
 
 const linkedList = new LinkedList ;
@@ -114,16 +128,46 @@ const findLengthOf = (link, node=link.head, length=1) => {
   return findLengthOf(link, node=node.next, length+1);
 };
 
-const findMiddleOf = (link) => {
-  return link.get(Math.floor(findLengthOf(link)/2));
+const findMiddleOf = (link, ptr1=link.head, ptr2=findLast(link), boolean) => {
+  if(ptr1.value === ptr2.value) return ptr1.value;
+  //if true, move ptr1
+  if(boolean){
+    ptr1 = ptr1.next;
+  }
+  //if false move ptr2
+  else{
+    ptr2 = findPrevious(link, ptr2.value);
+  }
+  return findMiddleOf(link, ptr1, ptr2, !boolean);
 };
-// console.log(findMiddleOf(linkedList));
 
+console.log(findMiddleOf(linkedList));
 
+const getNode = (link, index) => {
+  let node = link.head;
+  for(let i = 0; i < index; i++){
+    node = node.next;
+  }
+  return node;
+};
 
 //Write an algorithm to find the third element from the end of a linked list without using the .length property
 const findThirdFromEnd = (link) => {
   return link.get(findLengthOf(link) - 3);
 };
 
-console.log(findThirdFromEnd(linkedList));
+// console.log(findThirdFromEnd(linkedList));
+
+// const reverse = (link) => {
+//   let node = getNode(link, findLengthOf(link) - 1);
+//   for(let i = 0; i < findLengthOf(link); i++){
+//     if(i === findLengthOf(link) - 1){
+//       node.next = null;
+//     }else{
+//       node.next = getNode(link, findLengthOf(link) - i);
+//     }
+//   }
+//   return link;
+// };
+
+// console.log(reverse(linkedList));

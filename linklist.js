d5b3666242cbfb1eae7ccd225d44ce5a22cdd8ff
Jamie Wang <sephona@gmail.com>
2017-07-27 11:20:47 -0400

    Exercise 1 completed

--- linklist.js
+++ linklist.js
@@ -1,62 +1,75 @@
+'use strict';
+
 class LinkedList {
-    constructor() {
-        this.length = 0;
-        this.head = null;
+  constructor() {
+    this.length = 0;
+    this.head = null;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      throw new Error('Index error');
     }
 
-     insert(index, value) {
-        if (index < 0 || index > this.length) {
-            throw new Error('Index error');
-        }
+    const newNode = {
+      value
+    };
 
-        const newNode = {
-            value
-        };
-
-        if (index == 0) {
-            newNode.next = this.head;
-            this.head = newNode;
-        }
-        else {
+    if (index == 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    else {
             // Find the node which we want to insert after
-            const node = this._find(index - 1);
-            newNode.next = node.next;
-            node.next = newNode;
-        }
-
-        this.length++;
+      const node = this._find(index - 1);
+      newNode.next = node.next;
+      node.next = newNode;
     }
 
-    _find(index) {
-        let node = this.head;
-        for (let i=0; i<index; i++) {
-            node = node.next;
-        }
-        return node;
+    this.length++;
+  }
+
+  _find(index) {
+    let node = this.head;
+    for (let i=0; i<index; i++) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
     }
 
-    get(index) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
+    return this._find(index).value;
+  }
 
-        return this._find(index).value;
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
     }
 
-    remove(index) {
-        if (index < 0 || index >= this.length) {
-            throw new Error('Index error');
-        }
-
-        if (index == 0) {
-            this.head = this.head.next;
-        }
-        else {
+    if (index == 0) {
+      this.head = this.head.next;
+    }
+    else {
             // Find the node before the one we want to remove
-            const node = this._find(index - 1);
-            node.next = node.next.next;
-        }
-
-        this.length--;
+      const node = this._find(index - 1);
+      node.next = node.next.next;
     }
+
+    this.length--;
+  }
 }
+
+//Exercise 2
+
+// Implement the following functions that supplements any Linked List operation. Note that these are functions that are not part of the Linked List class, so implement them outside the linked list class.
+
+// display: displays the linked list (you may also name this PrintList) - You should use this function for other exercises to show the content of your list.
+// size: returns the size of the linked list
+// isEmpty: finds if the list is empty or not
+// findPrevious: finds the node before the item you are looking for
+// findLast: returns the last node in the linked list
+
